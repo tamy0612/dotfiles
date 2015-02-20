20b734b10dcd1ccf23dd80250e8b809409f3f28d
tamy0612 <fakey.insanity@gmail.com>
2015-02-20 23:39:17 +0900

    [update] zshenv

--- zsh/env/mac.zsh
+++ zsh/env/mac.zsh
@@ -1,16 +1,40 @@
+## Exports
+# lang
+export LANG=ja_JP.UTF-8
+export KCODE=u
+
+
+# path
+path=(
+  /usr/local/bin(N-/)
+  /Applications/MAMP/bin/php/php5.4.10/bin(N-/)
+  /Applications/pTeX.app/teTeX/bin(N-/)
+  $HOME/Library/TeXShop/bin(N-/)
+  /Applications/Octave.app/Contents/Resources/bin(N-/)
+  ${path}
+)
+
 # homebrew
-export BREW_HOME=/usr/local/Cellar
-
-# D lang
-export D_HOME=$BREW_HOME/dmd/`ls $BREW_HOME/dmd | awk 'END{print $NF}'`
-export D_IMPORT_DIR=/usr/local/include/d2
-
-# php
-path=(/Applications/MAMP/bin/php/php5.4.10/bin ${path})
+if type brew > /dev/null 2>&1 ; then
+  export BREW_HOME=/usr/local/Cellar
+fi
 
 # rsense
-export RSENSE_HOME=$BREW_HOME/rsense/0.3
+if brew --prefix rsense > /dev/null 2>&1 ; then
+  export RSENSE_HOME=`brew --prefix rsense`
+fi
 
+# set R_HOME
+if type R > /dev/null 2>&1 ; then
+  export R_HOME=/Library/Frameworks/R.framework/Resources
+fi
+
+# curl setting
+if [ -f $HOME/.curl/ca-bundle.crt ]; then
+  export CURL_CA_BUNDLE=$HOME/.curl/ca-bundle.crt
+fi
+
+## Alias
 # vim
 export EDITOR=/Applications/MacVim.app/Contents/MacOS/Vim
 alias vim='env LANG=ja_JP.UTF-8 /Applications/MacVim.app/Contents/MacOS/Vim'
@@ -18,27 +42,5 @@ alias vi='vim'
 alias gvim='/Applications/MacVim.app/Contents/MacOS/MacVim'
 alias vimdiff='/Applications/MacVim.app/Contents/MacOS/vimdiff'
 
-# latex
-path=(
-  /Applications/pTeX.app/teTeX/bin
-  /usr/local/bin
-  $HOME/Library/TeXShop/bin
-  ${path}
-)
-
-# octave setting
-path=(
-  /Applications/Octave.app/Contents/Resources/bin
-  ${path}
-)
-
-# set R_HOME
-export R_HOME=/Library/Frameworks/R.framework/Resources
-
-# curl setting
-if [ -f $HOME/.curl/ca-bundle.crt ]; then
-  export CURL_CA_BUNDLE=$HOME/.curl/ca-bundle.crt
-fi
-
-# Alias
+# rm
 alias rm='rmtrash'
