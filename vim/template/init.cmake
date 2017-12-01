06e1c9b432deb0f3e782ceb7ccb8b7a7205a3935
Yasumasa Tamura <tamy@Elsa.local>
2017-12-01 16:11:23 +0900

    modify cmake template

--- vim/template/init.cmake
+++ vim/template/init.cmake
@@ -1,8 +1,9 @@
 :%s/<+PATH+>/\=expand('%')/g
 :%s/<+DATE+>/\=strftime("%d %b. %Y")/g
+:%s/<+CMAKE_VERSION+>/\=get(g:,'neoformat_cmake_version','3.9')/g
 :5
 # <+PATH+>
 # Generated: <+DATE+>.
-# Last Change: <+DATE+>.
-cmake_minimum_required(VERSION 3.9)
+# Last Change: 27 Nov. 2017.
+cmake_minimum_required(VERSION <+CMAKE_VERSION+>)
 
