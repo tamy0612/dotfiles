55c86609bbc570db89e73ea877c5b86ef56fecc6
Yasumasa Tamura <tamy@Elsa.local>
2017-12-11 16:34:42 +0900

    add namespace declaration to C/C++ header template

--- vim/template/init.h
+++ vim/template/init.h
@@ -1,12 +1,21 @@
 :%s/<+FILE+>/\=expand("%:s?^src\/??:p:.")/g
 :%s/<+GUARD+>/\=toupper(expand("%:gs?\\.?_?:s?^src\/??:gs?\/?_?:p:."))/g
-:7
+:%s/<+NAMESPACE+>/\=tolower(join(filter(extend([get(g:, 'neoformat_project_name', '')], split(expand('%:s?^src\/??:p:.:h'), '\/')), 'v:val !=? ""'), '::'))/g
+:11
 /**
  * @file <+FILE+>
  */
 #ifndef <+GUARD+>
 #define <+GUARD+>
 
+#ifdef __cplusplus
+namespace <+NAMESPACE+> {
+#endif
 
 
-#endif  /* <+GUARD+> */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* <+GUARD+> */
