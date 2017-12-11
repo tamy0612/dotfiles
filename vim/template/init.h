a44a783f2577725c3387336bf6dfc218d9705548
Yasumasa Tamura <tamy@Elsa.local>
2017-12-11 16:37:24 +0900

    modify variable name

--- vim/template/init.h
+++ vim/template/init.h
@@ -1,6 +1,6 @@
 :%s/<+FILE+>/\=expand("%:s?^src\/??:p:.")/g
 :%s/<+GUARD+>/\=toupper(expand("%:gs?\\.?_?:s?^src\/??:gs?\/?_?:p:."))/g
-:%s/<+NAMESPACE+>/\=tolower(join(filter(extend([get(g:, 'neoformat_project_name', '')], split(expand('%:s?^src\/??:p:.:h'), '\/')), 'v:val !=? ""'), '::'))/g
+:%s/<+NAMESPACE+>/\=tolower(join(filter(extend([get(g:, 'template_project_name', '')], split(expand('%:s?^src\/??:p:.:h'), '\/')), 'v:val !=? ""'), '::'))/g
 :11
 /**
  * @file <+FILE+>
