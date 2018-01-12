145e30baab38f1e642a12370ee37928341de8467
Yasumasa Tamura <tamura@c.titech.ac.jp>
2018-01-12 16:31:52 +0900

    modify zsh prompt

--- zsh/rc/prompt.zsh
+++ zsh/rc/prompt.zsh
@@ -21,16 +21,17 @@ case "${TERM}" in
                 ;;
             256)
                 local ssh_style='%{[1;38;5;45m%}'
-                local status_style='%(?.[30;48;5;28m.[30;48;5;9m)%}'
-                local path_style='%{[1;38;5;250m%}'
-                local track_style='%{[1;38;5;45m%}'
+                local status_style='%(?.[1;38;5;82m.[1;38;5;9m)%}'
+                local path_style='%{[1;38;5;39m%}'
+                local track_style='%{[1;38;5;245m%}'
                 local track_stagestr_style='%{[1;38;5;11m%}'
                 local track_unstagestr_style='%{[1;38;5;9m%}'
                 ;;
             *)
                 ;;
         esac
-        local prompt_status='${status_style} ${reset_style} '
+        local status_char='%(?.✔︎.✖︎)'
+        local prompt_status='${status_style}${status_char} ${reset_style} '
         local prompt_ssh=''
         if [ -n "${SSH_CLIENT}${SSH_CONNECTION}" ]; then
             prompt_ssh='${ssh_style}[ssh://%n@%M] ${reset_style}'
@@ -44,15 +45,11 @@ case "${TERM}" in
         zstyle ':vcs_info:*' formats '${track_style}%s::%b%u%c'
         zstyle ':vcs_info:*' actionformats '${track_style}%s::%b%u%c-%a'
         update_prompt(){
-            local prompt_vcs_info=""
             LANG=en_US.UTF-8 vcs_info
-            # if [ ! -z ${vcs_info_msg_0_} ]; then
-            #     prompt_vcs_info="${track_style}${vcs_info_msg_0_}"
-            # fi
             PROMPT="
 ${prompt_status}${prompt_ssh}${prompt_path}${vcs_info_msg_0_}${reset_style}
 %(!.#.>) "
         }
         add-zsh-hook precmd update_prompt
-        PROMPT2="| "
+        PROMPT2="%(!.#.|)> "
 esac
