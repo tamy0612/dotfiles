d0d5389fc118994a3f0ea6a72f64a342706640b5
Yasumasa Tamura <tamura@c.titech.ac.jp>
2021-06-21 18:04:45 +0900

    add FZF CTRL-T option

--- zsh/rc/command.zsh
+++ zsh/rc/command.zsh
@@ -13,3 +13,12 @@ fi
 if type gotop > /dev/null 2>&1 ; then
     alias top=gotop
 fi
+
+if type fzf > /dev/null 2>&1 ; then
+    if type rg > /dev/null 2>&1 ; then
+        export FZF_CTRL_T_COMMAND='rg --files --hidden --follow --glob "!.git/*" --glob "!.ccls-cache/*"'
+    fi
+    if type bat > /dev/null 2>&1 ; then
+        export FZF_CTRL_T_OPTS='--preview "bat --color=always --style=header,grid --line-range :100 {}"'
+    fi
+fi
