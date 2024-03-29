889700ce44ebf23d31ab9aa112f7cc75aea0d79a
tamy <northern.beerfreak@gmail.com>
2024-03-29 16:50:52 +0900

    add wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require 'wezterm'
 local config = {}
+local hostname = wezterm.hostname()
 
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
     config.default_prog = {'wsl.exe', '--exec', '/bin/zsh', '-l'}
@@ -7,11 +8,20 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
 end
 
 config.color_scheme = 'AyuMirage (Gogh)'
+config.colors = {
+    cursor_bg = '#888888',
+    cursor_fg = '#222222',
+}
 
 config.font = wezterm.font_with_fallback {
     "Cica",
     "Ricty Diminished"
 }
+if hostname == 'Sabrina.local' then
+    config.font_size = 14
+else
+    config.font_size = 12
+end
 
 config.leader = { key = 'q', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.keys = {
